b1d366cf2a9055cae97ced4d93044292e6d99783
Oskar Remvang <oskarlr@stud.ntnu.no>
2021-02-27 10:47:27 +0100

    Lagde klassen RemoveException, implementerte remove metode i Department klassen, lagde tester til remove metoden

---
+++ src/main/java/edu/ntnu/idatt2001/RemoveException.java
@@ -0,0 +1,10 @@
+package edu.ntnu.idatt2001;
+
+public class RemoveException extends Exception{
+
+    private final long serialVersionUID = 1L;
+
+    public RemoveException(String message) {
+        super(message);
+    }
+}
---
+++ src/test/java/edu/ntnu/idatt2001/DepartmentTest.java
@@ -0,0 +1,55 @@
+package edu.ntnu.idatt2001;
+
+import org.junit.jupiter.api.*;
+
+import java.util.ArrayList;
+
+class DepartmentTest {
+    Patient patient = new Patient("Tore","Tang","12345","Gammel mann");
+    Employee employee = new Employee("Akka","Bakka","54321");
+
+    Department department = new Department("Department");
+
+    @Nested
+    @DisplayName("Testing if method removes employee and patient when the actually exist in the department")
+    class positiveTests{
+
+        @Test
+        void removePatientWhenPresent() {
+            department.addPatient(patient);
+            try {
+                department.remove(patient);
+            } catch (RemoveException e) {
+                e.printStackTrace();
+            }
+            Assertions.assertEquals(department.getPatients().size(), 0);
+        }
+
+        @Test
+        void removeEmployeeWhenPresent() {
+            department.addEmployee(employee);
+            try {
+                department.remove(employee);
+            } catch (RemoveException e) {
+                e.printStackTrace();
+            }
+            Assertions.assertEquals(department.getEmployees().size(), 0);
+        }
+    }
+
+    @Nested
+    @DisplayName("Testing if method throws exception when employee and patient does not exist in the department")
+    class negativeTests{
+        @Test
+        void removePatientWhenAbsent() {
+            department.getPatients().clear();
+            Assertions.assertThrows(RemoveException.class, () -> department.remove(patient));
+        }
+
+        @Test
+        void removeEmployeeWhenAbsent() {
+            department.getEmployees().clear();
+            Assertions.assertThrows(RemoveException.class, () -> department.remove(employee));
+        }
+    }
+}
